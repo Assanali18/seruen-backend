45394fdd4474e2bfc733311eeae6712ebe4dbd09
Assanali18 <ustaevasanali@gmail.com>
2024-07-18 20:10:07 +0500

    ux/ui

--- backend/src/telegramBot.ts
+++ backend/src/telegramBot.ts
@@ -26,9 +26,15 @@ interface UserPreferences {
     url: string;
     chatId: string;
   };
-} 
+}
 
 const userSetupStages: { [chatId: string]: { stage: number, field?: string } } = {};
+
+const commandMenu = [
+  [{ text: 'Изменить бюджет', callback_data: '/change_spendingLimit' }],
+  [{ text: 'Изменить увлечения', callback_data: '/change_hobbies' }],
+  [{ text: 'Остановить сеанс', callback_data: '/stop_session' }],
+];
 
 bot.onText(/\/start/, async (msg) => {
   const chatId = msg.chat.id;
@@ -48,19 +54,18 @@ bot.onText(/\/start/, async (msg) => {
       await user.save();
       userSetupStages[chatId] = { stage: 0 };
 
-      await bot.sendMessage(chatId, `Добро пожаловать, ${userName} в Seruen! Мы очень рады, что вы присоединились к нам. Давайте немного познакомимся, и мы будем присылать вам персонализированные рекомендации по мероприятиям в вашем городе! Для начала введите свой email:`);
+      await bot.sendMessage(chatId, `👋 Добро пожаловать, *${userName}*, в Seruen! Мы очень рады, что вы присоединились к нам. Давайте немного познакомимся, и мы будем присылать вам персонализированные рекомендации по мероприятиям в вашем городе! Для начала введите свой email:`, { parse_mode: 'Markdown' });
     } else {
       user.chatId = chatId.toString();
       user.stopSession = false;
       await user.save();
 
-      const welcomeMessage = `Добро пожаловать, ${userName} в Seruen! Мы очень рады, что вы присоединились к нам. Теперь мы будем присылать вам персонализированные рекомендации по мероприятиям в вашем городе!`;
+      const welcomeMessage = `👋 Добро пожаловать, *${userName}*, в Seruen! Мы очень рады, что вы присоединились к нам. Теперь мы будем присылать вам персонализированные рекомендации по мероприятиям в вашем городе!`;
 
-      await bot.sendMessage(chatId, welcomeMessage);
+      await bot.sendMessage(chatId, welcomeMessage, { parse_mode: 'Markdown' });
+      await bot.sendMessage(chatId, 'Мы готовим для вас рекомендации. Они начнут приходить очень скоро!', { parse_mode: 'Markdown' });
 
       try {
-        await bot.sendMessage(chatId, 'Мы готовим для вас первые рекомендации. Это может занять несколько минут...');
-
         const events = await EventModel.find();
         const CHUNK_SIZE = 10;
         const eventChunks = getEventChunks(events, CHUNK_SIZE);
@@ -85,29 +90,28 @@ bot.onText(/\/start/, async (msg) => {
         console.log('DB RECOMMENDATIONS', user.recommendations);
         await user.save();
 
-        await bot.sendMessage(chatId, 'Мы завершили подготовку ваших рекомендаций.');
+        await bot.sendMessage(chatId, '✨ Мы завершили подготовку ваших рекомендаций. Наслаждайтесь мероприятиями!', { parse_mode: 'Markdown' });
 
-        // Отправляем первый ивент
         await sendNextEvent(chatId);
 
       } catch (error) {
         console.error(`Ошибка при получении рекомендаций для chatId ${chatId}:`, error);
-        await bot.sendMessage(chatId, 'Извините, произошла ошибка при получении рекомендаций.');
+        await bot.sendMessage(chatId, '❌ Извините, произошла ошибка при получении рекомендаций.', { parse_mode: 'Markdown' });
       }
     }
   }
 });
 
-bot.onText(/\/change_spendingLimit/, async (msg) => {
+bot.onText(/\/change_budget/, async (msg) => {
   const chatId = msg.chat.id;
-  userSetupStages[chatId] = { stage: 0, field: 'spendingLimit' };
-  await bot.sendMessage(chatId, 'Пожалуйста, введите ваш новый бюджет (в тенге):');
+  userSetupStages[chatId] = { stage: 0, field: 'budget' };
+  await bot.sendMessage(chatId, 'Пожалуйста, введите ваш новый бюджет (в тенге):', { parse_mode: 'Markdown' });
 });
 
 bot.onText(/\/change_hobbies/, async (msg) => {
   const chatId = msg.chat.id;
   userSetupStages[chatId] = { stage: 0, field: 'hobbies' };
-  await bot.sendMessage(chatId, 'Пожалуйста, введите ваши новые увлечения (через запятую):');
+  await bot.sendMessage(chatId, 'Пожалуйста, введите ваши новые увлечения (через запятую):', { parse_mode: 'Markdown' });
 });
 
 bot.onText(/\/stop_session/, async (msg) => {
@@ -118,8 +122,9 @@ bot.onText(/\/stop_session/, async (msg) => {
     await user.save();
   }
   delete userSetupStages[chatId];
-  await bot.sendMessage(chatId, 'Сеанс завершен. Если захотите начать снова, используйте команду /start.');
+  await bot.sendMessage(chatId, '🛑 Сеанс завершен. Если захотите начать снова, используйте команду /start.', { parse_mode: 'Markdown' });
 });
+
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
@@ -129,7 +134,7 @@ bot.on('message', async (msg) => {
   const user = await User.findOne({ chatId });
 
   if (!user) {
-    await bot.sendMessage(chatId, 'Произошла ошибка. Пожалуйста, начните заново, используя команду /start.');
+    await bot.sendMessage(chatId, 'Произошла ошибка. Пожалуйста, начните заново, используя команду /start.', { parse_mode: 'Markdown' });
     return;
   }
 
@@ -142,7 +147,7 @@ bot.on('message', async (msg) => {
     case 'phone':
       user.phone = msg.text;
       break;
-    case 'spendingLimit':
+    case 'budget':
       user.spendingLimit = parseInt(msg.text);
       break;
     case 'hobbies':
@@ -153,33 +158,33 @@ bot.on('message', async (msg) => {
         case 0:
           user.email = msg.text;
           userSetupStages[chatId] = { stage: 1 };
-          await bot.sendMessage(chatId, 'Пожалуйста, введите ваш номер телефона:');
+          await bot.sendMessage(chatId, 'Пожалуйста, введите ваш номер телефона:', { parse_mode: 'Markdown' });
           return;
         case 1:
           user.phone = msg.text;
           userSetupStages[chatId] = { stage: 2 };
-          await bot.sendMessage(chatId, 'Пожалуйста, введите ваш бюджет (в тенге):');
+          await bot.sendMessage(chatId, 'Пожалуйста, введите ваш бюджет (в тенге):', { parse_mode: 'Markdown' });
           return;
         case 2:
           user.spendingLimit = parseInt(msg.text);
           userSetupStages[chatId] = { stage: 3 };
-          await bot.sendMessage(chatId, 'Пожалуйста, введите ваши увлечения (через запятую):');
+          await bot.sendMessage(chatId, 'Пожалуйста, введите ваши увлечения (через запятую):', { parse_mode: 'Markdown' });
           return;
         case 3:
           user.hobbies = msg.text.split(',').map(item => item.trim());
           await user.save();
           delete userSetupStages[chatId];
-          await bot.sendMessage(chatId, 'Спасибо! Ваши данные сохранены. Получаем ваши персонализированные рекомендации...');
+          await bot.sendMessage(chatId, 'Спасибо! Ваши данные сохранены. Получаем ваши персонализированные рекомендации...', { parse_mode: 'Markdown' });
           break;
       }
   }
 
   await user.save();
   delete userSetupStages[chatId];
-  await bot.sendMessage(chatId, 'Ваши данные обновлены. Получаем ваши персонализированные рекомендации...');
+  await bot.sendMessage(chatId, 'Ваши данные обновлены. Получаем ваши персонализированные рекомендации...', { parse_mode: 'Markdown' });
 
   try {
-    bot.sendMessage(chatId, 'Готовим для Вас рекомендации...');
+    bot.sendMessage(chatId, 'Готовим для Вас рекомендации...', { parse_mode: 'Markdown' });
     const events = await EventModel.find();
     const CHUNK_SIZE = 10;
     const eventChunks = getEventChunks(events, CHUNK_SIZE);
@@ -204,23 +209,22 @@ bot.on('message', async (msg) => {
     console.log('DB RECOMMENDATIONS', user.recommendations);
     await user.save();
 
-    await bot.sendMessage(chatId, 'Мы завершили подготовку ваших рекомендаций.');
+    await bot.sendMessage(chatId, '✨ Мы завершили подготовку ваших рекомендаций. Наслаждайтесь мероприятиями!', { parse_mode: 'Markdown' });
 
-    // Отправляем первый ивент
     await sendNextEvent(chatId);
 
   } catch (error) {
     console.error(`Ошибка при получении рекомендаций для chatId ${chatId}:`, error);
-    await bot.sendMessage(chatId, 'Извините, произошла ошибка при получении рекомендаций.');
+    await bot.sendMessage(chatId, '❌ Извините, произошла ошибка при получении рекомендаций.', { parse_mode: 'Markdown' });
   }
 });
 
-// Функция для отправки следующего ивента
+
 const sendNextEvent = async (chatId: number) => {
   const user = await User.findOne({ chatId });
 
   if (!user || !user.recommendations || user.recommendations.length === 0) {
-    await bot.sendMessage(chatId, 'У вас нет рекомендаций. Пожалуйста, сначала получите рекомендации.');
+    await bot.sendMessage(chatId, 'У вас нет рекомендаций. Пожалуйста, сначала получите рекомендации.', { parse_mode: 'Markdown' });
     return;
   }
 
@@ -231,11 +235,12 @@ const sendNextEvent = async (chatId: number) => {
   const nextEvent = user.recommendations[user.lastRecommendationIndex || 0];
 
   if (!nextEvent) {
-    await bot.sendMessage(chatId, 'No more events to show.');
+    await bot.sendMessage(chatId, 'No more events to show.', { parse_mode: 'Markdown' });
     return;
   }
 
   await bot.sendMessage(chatId, nextEvent.message.replace(/\\n/g, '\n'), {
+    parse_mode: 'Markdown',
     reply_markup: {
       inline_keyboard: [
         [{ text: "Купить билеты", callback_data: `buy_ticket_${user.lastRecommendationIndex}` }],
@@ -262,18 +267,18 @@ bot.on('callback_query', async (callbackQuery) => {
     const user = await User.findOne({ chatId });
     const event = user?.recommendations?.[index];
     if (event) {
-      await bot.sendMessage(chatId, `Покупка билета для мероприятия: ${event.venue}`);
+      await bot.sendMessage(chatId, `Покупка билета для мероприятия: ${event.venue}`, { parse_mode: 'Markdown' });
       if (event.ticketLink) {
-        await bot.sendMessage(chatId, 'Покупаем билет, пожалуйста подождите...');
+        await bot.sendMessage(chatId, 'Покупаем билет, пожалуйста подождите...', { parse_mode: 'Markdown' });
         try {
           await buyTickets(event.ticketLink, chatId, bot);
-          await bot.sendMessage(chatId, 'Билеты успешно куплены!');
+          await bot.sendMessage(chatId, '🎫 Билеты успешно куплены!', { parse_mode: 'Markdown' });
         } catch (error) {
           console.error(`Ошибка при покупке билетов для chatId ${chatId}:`, error);
-          await bot.sendMessage(chatId, 'Извините, произошла ошибка при покупке билетов.');
+          await bot.sendMessage(chatId, '❌ Извините, произошла ошибка при покупке билетов.', { parse_mode: 'Markdown' });
         }
       } else {
-        await bot.sendMessage(chatId, 'Извините, ссылка на билет недоступна.');
+        await bot.sendMessage(chatId, 'Извините, ссылка на билет недоступна.', { parse_mode: 'Markdown' });
       }
     }
   }
