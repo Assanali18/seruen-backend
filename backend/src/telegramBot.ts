37638b057100a14e1d86075889b467333cf795a2
Assanali18 <ustaevasanali@gmail.com>
2024-07-18 12:33:30 +0500

    fix

--- backend/src/telegramBot.ts
+++ backend/src/telegramBot.ts
@@ -6,7 +6,7 @@ import buyTickets from './buyTickets';
 import EventModel from './event/models/Event';
 
 // CHANGE TOKEN IF YOU DEPLOY
-const TELEGRAM_TOKEN = process.env.TELEGRAM_BOT_TOKEN_DEV2 || '';
+const TELEGRAM_TOKEN = process.env.TELEGRAM_BOT_TOKEN_DEV || '';
 if (!TELEGRAM_TOKEN) {
   throw new Error('TELEGRAM_TOKEN is not set');
 }
